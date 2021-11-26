dc191ca0cfdd1f230b485ac773bb1d5a235a0e81
Thomas Burton <tburton5572@gmail.com>
2021-11-26 16:33:44 +0000

    clean-up of poisson solver tests

--- src/PoissonSolver.jl
+++ src/PoissonSolver.jl
@@ -5,7 +5,6 @@ import LinearAlgebra
 include("eye.jl")
 include("laplace.jl")
 
-# TODO: BCs need to take in the desired differentiation matrix as well
-# TODO: Tests are broken, need to import chebyshev stuff from another place
+# TODO: inhomogeneous BCs
 
 end
--- test/test_laplace.jl
+++ test/test_laplace.jl
@@ -35,7 +35,7 @@
     @test laplace_neumann_recon[rinner, :] ≈ double_diffmat[rinner, :]
 
     # -------------------------------------------------------------------------
-    # Simple Dirichlet
+    # Initialise Poisson solver
     # -------------------------------------------------------------------------
     # intiialise constants
     β2 = 1.0
@@ -49,113 +49,15 @@
     D2 = chebdiff(Ny2); DD2 = chebddiff(Ny2)
     y = chebpts(Ny2)
 
-    # # initialise grid
+    # initialise grid
     grid = Grid(y, Nz2, Nt2, D2, DD2, rand(Ny2))
 
-    # # initialise solution field
-    ϕ1_spec = SpectralField(grid)
-    ϕ1_phys = PhysicalField(grid)
-
-    # # initialise RHS field
-    rhs1_spec = SpectralField(grid)
-    rhs1_phys = PhysicalField(grid, rhs1_fun)
-
-    # # initialise FFT plans
-    FFT = FFTPlan!(ϕ1_phys, flags = FFTW.ESTIMATE)
-    IFFT = IFFTPlan!(ϕ1_spec, flags = FFTW.ESTIMATE)
-
-    # # populate spectral version of RHS
-    FFT(rhs1_spec, rhs1_phys)
-
-    # # initialise Laplace operator
+    # initialise Laplace operators
     laplace1 = Laplace(Ny2, Nz2, β2, DD2)
-
-    # # find solution
-    solve!(ϕ1_spec, laplace1, rhs1_spec)
-    IFFT(ϕ1_phys, ϕ1_spec)
-    @test ϕ1_phys ≈ PhysicalField(grid, sol1_fun)
-
-    # -------------------------------------------------------------------------
-    # Simple Neumann
-    # -------------------------------------------------------------------------
-    # initialise functions
-    sol2_fun(y, z, t) = y*(((y^2)/3) - 1)
-    rhs2_fun(y, z, t) = 2*y
-
-    # initialise solution field
-    ϕ2_spec = SpectralField(grid)
-    ϕ2_phys = PhysicalField(grid)
-
-    # initialise RHS field
-    rhs2_spec = SpectralField(grid)
-    rhs2_phys = PhysicalField(grid, rhs2_fun)
-
-    # populate spectral version of RHS
-    FFT(rhs2_spec, rhs2_phys)
-
-    # initialise Laplace operator
     laplace2 = Laplace(Ny2, Nz2, β2, DD2, D2)
 
-    # find solution
-    solve!(ϕ2_spec, laplace2, rhs2_spec)
-    IFFT(ϕ2_phys, ϕ2_spec)
-
-    # modify solution with offset (arbitrary when using Neumann BCs)
-    ϕ2_phys = ϕ2_phys .- (ϕ2_phys[1, 1, 1] - PhysicalField(grid, sol2_fun)[1, 1, 1])
-    @test ϕ2_phys ≈ PhysicalField(grid, sol2_fun)
-
     # -------------------------------------------------------------------------
-    # Complex Dirichlet
-    # -------------------------------------------------------------------------
-    # initialise functions
-    sol3_fun(y, z, t) = (1 - y^2)*exp(cos(z))
-    rhs3_fun(y, z, t) = -2*exp(cos(z)) + (sin(z)^2 - cos(z))*(1 - y^2)*exp(cos(z))
-
-    # initialise solution field
-    ϕ3_spec = SpectralField(grid)
-    ϕ3_phys = PhysicalField(grid)
-
-    # initialise RHS field
-    rhs3_spec = SpectralField(grid)
-    rhs3_phys = PhysicalField(grid, rhs3_fun)
-
-    # populate spectral version of RHS
-    FFT(rhs3_spec, rhs3_phys)
-
-    # find solution
-    solve!(ϕ3_spec, laplace1, rhs3_spec)
-    IFFT(ϕ3_phys, ϕ3_spec)
-
-    @test ϕ3_phys ≈ PhysicalField(grid, sol3_fun)
-
-    # -------------------------------------------------------------------------
-    # Complex Neumann
-    # -------------------------------------------------------------------------
-    # initialise functions
-    sol4_fun(y, z, t) = y*(((y^2)/3) - 1)*exp(cos(z))
-    rhs4_fun(y, z, t) = 2*y*exp(cos(z)) + (sin(z)^2 - cos(z))*y*(((y^2)/3) - 1)*exp(cos(z))
-
-    # initialise solution field
-    ϕ4_spec = SpectralField(grid)
-    ϕ4_phys = PhysicalField(grid)
-
-    # initialise RHS field
-    rhs4_spec = SpectralField(grid)
-    rhs4_phys = PhysicalField(grid, rhs4_fun)
-
-    # populate spectral version of RHS
-    FFT(rhs4_spec, rhs4_phys)
-
-    # find solution
-    solve!(ϕ4_spec, laplace2, rhs4_spec)
-    IFFT(ϕ4_phys, ϕ4_spec)
-
-    # modify solution with offset (arbitrary when using Neumann BCs)
-    ϕ4_phys = ϕ4_phys .- (ϕ4_phys[1, 1, 1] - PhysicalField(grid, sol4_fun)[1, 1, 1])
-    @test ϕ4_phys ≈ PhysicalField(grid, sol4_fun)
-
-    # -------------------------------------------------------------------------
-    # Super Complex Dirichlet
+    # Dirichlet
     # -------------------------------------------------------------------------
     # initialise functions
     sol5_fun(y, z, t) = (1 - y^2)*exp(cos(z))*atan(sin(t))
@@ -164,6 +66,10 @@
     # initialise solution field
     ϕ5_spec = SpectralField(grid)
     ϕ5_phys = PhysicalField(grid)
+
+    # initialise FFT plans
+    FFT = FFTPlan!(ϕ5_phys, flags = FFTW.ESTIMATE)
+    IFFT = IFFTPlan!(ϕ5_spec, flags = FFTW.ESTIMATE)
 
     # initialise RHS field
     rhs5_spec = SpectralField(grid)
@@ -179,7 +85,7 @@
     @test ϕ5_phys ≈ PhysicalField(grid, sol5_fun)
 
     # -------------------------------------------------------------------------
-    # Super Complex Neumann
+    # Neumann
     # -------------------------------------------------------------------------
     # initialise functions
     sol6_fun(y, z, t) = y*(((y^2)/3) - 1)*exp(cos(z))*atan(sin(t))
