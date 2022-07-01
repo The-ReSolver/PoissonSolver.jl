45bd70b2007960775d66abe8dc0c47f3ef7b6fac
Thomas Burton <tburton5572@gmail.com>
2022-07-01 13:40:25 +0100

    added benchmark script for solver

---
+++ perf/perf_poisson.jl
@@ -0,0 +1,28 @@
+# This script performs a benchmarking of the Poisson solver.
+
+using BenchmarkTools
+
+using PoissonSolver
+using Fields
+using ChebUtils
+using FDGrids
+
+# setup fields
+β = 1.0
+Ny = 64; Nz = 64; Nt = 64
+y = chebpts(Ny)
+# D2 = chebdiff(Ny); DD2 = chebddiff(Ny)
+D2 = DiffMatrix(y, 5, 1)
+DD2 = DiffMatrix(y, 5, 2)
+grid = Grid(y, Nz, Nt, D2, DD2, rand(Ny), β, 0.0)
+laplace_banded = Laplace(Ny, Nz, β, DD2, D2, :banded)
+laplace_lapack = Laplace(Ny, Nz, β, DD2, D2, :lapack)
+ϕ = SpectralField(grid)
+rhs = SpectralField(grid)
+BC = (zeros(ComplexF64, Nz, Nt), zeros(ComplexF64, Nz, Nt))
+
+@btime solve!($ϕ, $laplace_banded, $rhs, $BC)
+@btime solve!($ϕ, $laplace_lapack, $rhs, $BC)
+
+# NOTE: custom banded solver is faster for same grid size
+# NOTE: diff matrix implementation is faster as plot would imply
